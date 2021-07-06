bfffafc19470bcf0be55d5193930cb60cdab5558
Feliphe Rodrigues dos Santos <82689032+felipheucl@users.noreply.github.com>
2021-07-06 14:28:09 +0000

    Sucesso em enviar os dados do arquivo.txt para a lista encadeada.

--- main.cpp
+++ main.cpp
@@ -6,52 +6,35 @@ using namespace std;
 
 ifstream leitura;
 ofstream gravar;
-
 int loop;
-
-/*
-int matricula[51], investimentos[51], posts[51], matr, id, postsmatricula;
-int Download(){
-    leitura.open("banco_de_dados.txt", ios::app);
-    leitura>>matricula[0];
-    matr=matricula[0];
-    for (int i=1; i<51; i++) {
-        leitura>>matricula[i];
-        leitura>>investimentos[i];
-        leitura>>posts[i];
-    }
-    leitura.close();
-    return 0;
-}
-*/
+string txt;
 
 // as linhas abaixo foram extraidas do código da avaliação P3
 // trazem o STRUCT criado e a função para cálculo da hipotenusa
 struct tTriangulos
 {
-        tTriangulos (float l1, float l2, float l3): lado1(l1), lado2(l2), lado3(l3)
-        {
-        }
-
-        public:
-        float lado1, lado2, lado3;
-  
-        friend ostream& operator<<(ostream& os, const tTriangulos& tT)
-        {
-        os << "Medidas dos lados: " << tT.lado1 << ", " << tT.lado2 << ", " << tT.lado3 << endl;
-        return os;
-        }
+ tTriangulos (float l1, float l2, float l3): lado1(l1), lado2(l2), lado3(l3)
+ {
+ }
+ public:
+ float lado1, lado2, lado3;
+ 
+ friend ostream& operator<<(ostream& os, const tTriangulos& tT)
+ {
+  os << "Medidas dos lados: " << tT.lado1 << ", " << tT.lado2 << ", " << tT.lado3 << endl;
+  return os;
+ }
 };
 
 float Hipotenusa(float num1, float num2)
 {
-       float r;
-       r=sqrt(num1*num1 + num2*num2);
-       return r;
+ float r;
+ r=sqrt(num1*num1 + num2*num2);
+ return r;
 }
 // *****************************************
 
-
+// a partir desta linha estão as lógicas para a resolução do problema
 class No
 {
   public:
@@ -60,35 +43,34 @@ class No
 
   No(float elemento1, float elemento2, float elemento3)
   {
-  this->b=elemento1;
-  this->c=elemento2;
-  this->a=elemento3;
-  this->proximo=NULL;
-  }
-  
-  
-  float obterValor() //impressao na tela
-  {
-  cout<<b<<" - "<<c<<" - "<<a<<endl;
-  return b;
+   this->b=elemento1;
+   this->c=elemento2;
+   this->a=elemento3;
+   this->proximo=NULL;
   }
   
   float Uploadtxt() //gravar em arquivo txt
   {
-  gravar.open("banco_de_dados.txt", ios::app);
-  gravar<<b<<" "<<c<<" "<<a<<"\n";
-  gravar.close();
-  return b;
+   gravar.open("banco_de_dados.txt", ios::app);
+   gravar<<b<<"\n"<<c<<"\n"<<a<<"\n";
+   gravar.close();
+   return 0;
   }
 
+  float obterValor() //impressao na tela
+  {
+   cout<<b<<" "<<c<<" ";
+   return a;
+  }
+ 
   void definirProximo(No* end)
   {
-    proximo=end;
+   proximo=end;
   }
 
   No* obterProximo()
   {
-    return proximo;
+   return proximo;
   }
 };
 
@@ -97,33 +79,53 @@ class Lista
   public:
   No* inicio;
   No* fim;
-  No* ctrl=NULL;
+  No* ctrl = NULL;
 
   Lista()
   {
-    inicio = NULL;
-    fim=NULL;
+   inicio = NULL;
+   fim    = NULL;
   }
 
   Lista(float num1, float num2, float num3)
   {
-    inicio = new No(num1,num2,num3);
-    fim = inicio;
+   inicio = new No(num1,num2,num3);
+   fim = inicio;
+  }
+
+   float Download() // Lê o arquivo .TXT e passa os dados para a Lista Encadeada
+   {
+   int j=0;
+   float aux[]={0,0,0,0};    
+   leitura.open("banco_de_dados.txt", ios::in);
+   while(getline(leitura,txt)) // faz a leitura do arquivo . TXT, linha a linha
+   {
+    aux[j]=stof(txt); // "stof" converte string para float
+    j=j+1;            // lógica para passar de 3 em 3 as informações do .TXT
+    if (j==3)
+     {
+      insereNoFim(aux[0],aux[1],aux[2]);
+      j=0;
+     }
+    }  
+   leitura.close();
+   
+   return 0;
   }
 
   virtual ~Lista()
   {
-    delete inicio;
+   delete inicio;
   }
 
   bool seVazia()
   {
-    return (inicio == NULL);
+   return (inicio == NULL);
   }
 
   void imprimirElementos()
   {
-    cout<<"ELEMENTOS DA LISTA: "<<endl;
+    Download();
     No* i=inicio;
     if(seVazia())
     {
@@ -131,58 +133,58 @@ class Lista
     }
     else
     {
-      while(i)
-      {
-        cout<<i->obterValor()<<endl;
-        i = i->obterProximo();
-      }
+     while(i)
+     {
+      cout<<i->obterValor()<<endl;
+      i = i->obterProximo();
+     }
       cout<<endl;
     }
   }
 
-  void GravarElementos()
+ void GravarElementos()
+ {
+  No* i;
+  if (ctrl==NULL)
   {
-    No* i;
-    if (ctrl==NULL)
-     {
-     i=inicio;
-     }
-    else
-     {
-     i=ctrl;
-     i = i->obterProximo();    
-     }
+   i=inicio;
+  }
+  else
+  {
+   i=ctrl;
+   i = i->obterProximo();    
+  }
 
-    if(seVazia())
-     {
-     cout<<"Lista sem elementos!"<<endl;
-     }
-    else
-     {
-     while(i)
-      {
-      i->Uploadtxt();
-      i = i->obterProximo();
-      }
-      ctrl=fim;  
-      cout<<"Arquivo 'banco_de_dados.txt' atualizado!"<<endl;
-     }
-  }  
+  if(seVazia())
+  {
+   cout<<"Lista sem elementos!"<<endl;
+  }
+  else
+  {
+    while(i)
+    {
+     i->Uploadtxt();
+     i = i->obterProximo();
+    }
+    ctrl=fim;  
+    cout<<"Arquivo 'banco_de_dados.txt' atualizado!"<<endl;
+  }
+ }  
 
  void insereNoFim(float v1, float v2, float v3)
-  {
-    No* novo = new No(v1,v2,v3);
+ {
+  No* novo = new No(v1,v2,v3);
 
-    if(seVazia())
-    {
-     inicio = novo;
-     fim = novo;
-    }
-    else
-    {
-     fim -> definirProximo(novo);
-     fim = novo;
-    }
+  if(seVazia())
+  {
+   inicio = novo;
+   fim = novo;
+  }
+  else
+   {
+    fim -> definirProximo(novo);
+    fim = novo;
+   }
   }
 };
 
@@ -194,9 +196,8 @@ char escolha;
 float cateto1, cateto2;
 
 cout<<endl<<endl<<"TRABALHO PRÁTICO - Utilização de Lista e Arquivos"<<endl<<endl;
-
-
 cout<<"!!! Dados dos Triangulos Retangulos !!!"<<endl<<endl;
+
 cout<<endl<<"GRAVAR as informações      > tecle 'g'"<<endl;
 cout<<"VISUALIZAR informações     > tecle 'v'"<<endl<<endl;
 cout<<"FECHAR programa            > tecle 'x'"<<endl<<endl;
@@ -207,32 +208,30 @@ while (escolha=='g' || escolha=='v')
 {
     if (escolha=='g')
     {
-    cout<<"Quantos triagulos deseja cadastrar?  ";
-    cin>>loop;
-    for (int num=0; num<loop; num++)
+     cout<<"Quantos triagulos deseja cadastrar?  ";
+     cin>>loop;
+     for (int num=0; num<loop; num++)
         {
-        cout<<endl<<"Digite o valor do 1º cateto:"<<endl;
-        cin>>cateto1;
-        cout<<"Digite o valor do 2º cateto:"<<endl;
-        cin>>cateto2;
-        tTriangulos Triangulo=tTriangulos(cateto1,cateto2,Hipotenusa(cateto1,cateto2));
+         cout<<endl<<"Digite o valor do 1º cateto:"<<endl;
+         cin>>cateto1;
+         cout<<"Digite o valor do 2º cateto:"<<endl;
+         cin>>cateto2;
+         tTriangulos Triangulo=tTriangulos(cateto1,cateto2,Hipotenusa(cateto1,cateto2));
         
-        lTriangle.insereNoFim(cateto1,cateto2,Hipotenusa(cateto1,cateto2));
+         lTriangle.insereNoFim(cateto1,cateto2,Hipotenusa(cateto1,cateto2));
         }
-    lTriangle.GravarElementos();     
+     lTriangle.GravarElementos();     
     }
     else
     {
-      
+     lTriangle.imprimirElementos();
     }
   
-cout<<endl<<endl<<"GRAVAR as informações      > tecle 'g'"<<endl;
-cout<<"VISUALIZAR as informações  > tecle 'v'"<<endl<<endl;
-cout<<"FECHAR programa            > tecle 'x'"<<endl<<endl;
-cin>>escolha;
+ cout<<endl<<endl<<"GRAVAR as informações      > tecle 'g'"<<endl;
+ cout<<"VISUALIZAR as informações  > tecle 'v'"<<endl<<endl;
+ cout<<"FECHAR programa            > tecle 'x'"<<endl<<endl;
+ cin>>escolha;
     
 }
-
-cout<<endl<<"Obrigado !"<<endl;
-
+ cout<<endl<<"Obrigado !"<<endl;
 }    
